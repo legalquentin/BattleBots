8118523c1dc28bcc67bb1c1efbd1dbf9f519c0bd
quentin <legal.quentin@gmail.com>
2020-02-04 17:51:49 +0100

    feat: implemented stub in bot + refactor

--- app/api-go/game/constants.go
+++ app/api-go/game/constants.go
@@ -21,6 +21,24 @@ var availableBots = []Bot{
 	Bot{1, "192.168.1.31", false, false, "Razorback", 10, 4, 6, 10},
 }
 
+TABLE STREAMS
+stream_id, game_id, robot_id, kinesis_url, s3_url, private, running, duration, encodage
+
+TABLE GAMES
+game_id, game_name, arena_id, game_status, date_created
+
+TABLE ARENA
+arnena_id, arena_name, available
+
+TABLE ROBOTS
+bot_id, bot_ip, running, taken, name, speed, damage, fire_rate, health, armor, arena_id, player_id, game_id
+
+TABLE PLAYER_GAME
+game_id, player_id, winner(true/false), game_points
+
+TABLE PLAYER
+player_id, total_points
+
 var appartement = Environment{
 	1, "quentin's appartement", []*Bot{&availableBots[0], &availableBots[1]}, true,
 }
