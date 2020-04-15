6ed55e2235dc59d54b5665a9203676098ab18e4d
Lucas Pointurier <pointu_l@etna-alternance.net>
2020-04-15 17:46:48 +0200

    feat(bot-fat-and-light-client): crossover build + other improvements + prepare workspace for developements

---
+++ app/vuejs-desktop/src/shims-vue.d.ts
@@ -0,0 +1,13 @@
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
+
+// Just an exemple to add custom properties
+// declare module "vue/types/vue" {
+//   interface Vue {
+//     $worker: WWorker;
+//   }
+// }
+
+declare module "semantic-ui-vue";
