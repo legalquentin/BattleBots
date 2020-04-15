ea3c00ddec2599121ffe8e2112c1bf171002fd9a
Lucas Pointurier <pointu_l@etna-alternance.net>
2020-04-15 17:46:48 +0200

    feat(build-fat-and-light-client): crossover build + other improvements + prepare workspace for developements

---
+++ app/vuejs-desktop/src/shims-tsx.d.ts
@@ -0,0 +1,32 @@
+import Vue, { VNode } from 'vue'
+
+declare global {
+  namespace JSX {
+    // tslint:disable no-empty-interface
+    interface Element extends VNode {}
+    // tslint:disable no-empty-interface
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any;
+    }
+  }
+}
+
+type RequestIdleCallbackHandle = any;
+type RequestIdleCallbackOptions = {
+  timeout: number;
+};
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean;
+  timeRemaining: (() => number);
+};
+
+declare global {
+  interface Window {
+    requestIdleCallback: ((
+      callback: ((deadline: RequestIdleCallbackDeadline) => void),
+      opts?: RequestIdleCallbackOptions,
+    ) => RequestIdleCallbackHandle);
+    cancelIdleCallback: ((handle: RequestIdleCallbackHandle) => void);
+  }
+}
---
+++ app/vuejs-desktop/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    runtimeCompiler: true,
+    productionSourceMap: false,
+    outputDir: __dirname + "/../api-node/src/public/App/",
+    publicPath: process.env.BASE_URL,
+  }
