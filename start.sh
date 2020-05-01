dc87e31356fd3642fa3b4ff82ecc566a3037d932
Thomas SIMOES <simoes_t@etna-alternance.net>
2020-05-01 16:36:08 +0200

    fix start

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cd infra && make prepare && make test:battlebots && make deploy:front && cp -r ../app/API/src/public/App ../app/api-node/src/public  && make run:battlebots
+cd infra && make prepare && make deploy:front && cp -r ../app/API/src/public/App ../app/api-node/src/public  && make run:battlebots
