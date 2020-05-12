3b9b8ab0fa9e686c39b77df4dd36e584ef18c4fd
quentin <legal.quentin@gmail.com>
2020-05-12 12:26:42 +0200

    logging in game creation

--- app/api-node/src/service/impl/BattleWorkerService.ts
+++ app/api-node/src/service/impl/BattleWorkerService.ts
@@ -25,7 +25,7 @@ interface IWorkerMeta {
 }
 
 @Singleton
-export default class BattleWorkerService implements IBattleWorkerService{
+export default class BattleWorkerService implements IBattleWorkerService {
     // Workers hold ref to all running games
 
 
@@ -36,39 +36,46 @@ export default class BattleWorkerService implements IBattleWorkerService{
      * startGoWorker will start a go process on the server add it to the listof  "WorkerProcesses"
      */
     public async startGoWorker(game: IGameResource) {
-        const addr = this.config.getWorkerAddress();
-        const port = this.config.getWorkerPort();
-        const p = await new Promise(async rslv => {
-            const WORKER_PATH = `${this.config.getWorkerDir()}/main.go`; // '/home/quentin/go/src/TIC-GPE5/Worker';
-            const secret = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-            if (Workers == null) {
-                const child = cp.spawn('go', ['run', WORKER_PATH, secret], { stdio: [process.stdin, process.stdout, process.stderr] });
-                Workers = { process: child, url: addr + ":" + port, key: secret };
-                // give 3 sec to start the worker
-                await new Promise(resolve => setTimeout(resolve, 5000));
-            }
-            game.token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-            request({
-                agentOptions: { rejectUnauthorized: false },
-                body: {
-                    id: game.id,
-                    name: game.name,
-                    token: game.token
-                },
-                headers: {
-                    'x-api-key': secret
-                },
-                json: true,
-                method: "POST",
-                strictSSL: false,
-                url: `https://${addr}:${port}/api/game/create`
-            }, (err, res) => {
-                console.log("#################################");
-                console.log(res.body);
-                rslv({ token: game.token, secret: secret });
+        try {
+            const addr = this.config.getWorkerAddress();
+            const port = this.config.getWorkerPort();
+            const p = await new Promise(async rslv => {
+                const WORKER_PATH = `${this.config.getWorkerDir()}/main.go`; // '/home/quentin/go/src/TIC-GPE5/Worker';
+                const secret = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+                if (Workers == null) {
+                    const child = cp.spawn('go', ['run', WORKER_PATH, secret], { stdio: [process.stdin, process.stdout, process.stderr] });
+                    Workers = { process: child, url: addr + ":" + port, key: secret };
+                    // give 3 sec to start the worker
+                    await new Promise(resolve => setTimeout(resolve, 5000));
+                }
+                game.token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+                request({
+                    agentOptions: { rejectUnauthorized: false },
+                    body: {
+                        id: game.id,
+                        name: game.name,
+                        token: game.token
+                    },
+                    headers: {
+                        'x-api-key': secret
+                    },
+                    json: true,
+                    method: "POST",
+                    strictSSL: false,
+                    url: `https://${addr}:${port}/api/game/create`
+                }, (err, res) => {
+                    console.log("#################################");
+                    console.log(res.body);
+                    rslv({ token: game.token, secret: secret });
+                });
             });
-        });
-        return p;
+            return p;
+        } catch (e) {
+            console.log(e);
+            return {
+                err: JSON.stringify(e)
+            }
+        }
     }
 
     // kill package from tree-kill kill all suprocess started from one
