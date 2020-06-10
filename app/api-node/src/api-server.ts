ea4127dc7df8f5abfc6de33b8e5efdc33cad524f
simoes_t <simoes_t@etna-alternance.net>
2020-06-10 20:03:07 +0200

    fix:load file

--- app/api-node/src/api-server.ts
+++ app/api-node/src/api-server.ts
@@ -30,8 +30,7 @@ export abstract class ApiServer {
         Container.configure(iocConfig);
         Container.environment(connectionName());
         this.config();
-        Server.loadServices(this.app, 'controller/**/*.js', __dirname);
-        Server.loadServices(this.app, 'controller/**/*.ts', __dirname);
+        Server.loadServices(this.app, 'controller/**/*.{js,ts}', __dirname);
         Server.swagger(this.app, { 
             swaggerUiOptions: {
                 customSiteTitle: 'BattleBots'
