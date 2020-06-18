3c815dfb9180baf5cc329711fb93af1e40e890f3
Lucas Pointurier <pointu_l@etna-alternance.net>
2020-06-18 18:27:17 +0200

    real-fix

--- app/vuejs-desktop/src/shims-vue.d.ts
+++ app/vuejs-desktop/src/shims-vue.d.ts
@@ -1,16 +1,9 @@
-import ConnectionManager from './utils/ConnectionManager'
-
 declare module '*.vue' {
-  import Vue from 'vue'
+  import Vue from 'vue';
   
   export default Vue
 }
 
 
-declare module "vue/types/vue" {
-  interface Vue {
-    connectionManager: ConnectionManager;
-  }
-}
 
 declare module "semantic-ui-vue";
