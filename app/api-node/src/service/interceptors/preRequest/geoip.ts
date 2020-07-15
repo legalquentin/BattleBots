3f5b4267e415f81c7702e2b0d6cc088519ce1b77
simoes_t <simoes_t@etna-alternance.net>
2020-07-15 22:49:16 +0200

    fix:insert geoip

--- app/api-node/src/service/interceptors/preRequest/geoip.ts
+++ app/api-node/src/service/interceptors/preRequest/geoip.ts
@@ -7,12 +7,15 @@ export async function preRequestGeoIp(req: any){
     const LOCAL_ADDRESS = "127.0.0.1";
 
     try {
+        let geoip = null;
+
         if (req.socket.remoteAddress !== LOCAL_ADDRESS){
-            const geoip = await geoIpService.findByIp(req.socket.remoteAddress);
+            geoip = await geoIpService.findByIp(req.socket.remoteAddress);
 
             if (geoip == null){
-                req.geoip = await geoIpService.getInfo(req.socket.remoteAddress);
+                geoip = await geoIpService.getInfo(req.socket.remoteAddress);
             }
+            await geoIpService.save(geoip);
         }
     }
     catch (e){
