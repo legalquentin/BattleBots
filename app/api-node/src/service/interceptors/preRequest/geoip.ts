bf2e6ab810b83514e5c8266f7a421a83e2d24bc0
simoes_t <simoes_t@etna-alternance.net>
2020-07-21 16:11:12 +0200

    fix:end game

--- app/api-node/src/service/interceptors/preRequest/geoip.ts
+++ app/api-node/src/service/interceptors/preRequest/geoip.ts
@@ -4,17 +4,12 @@ import { GeoIpService } from "../../GeoIpService";
 
 export async function preRequestGeoIp(req: any){
     const geoIpService: GeoIpService = Container.get(GeoIpService);
-    const LOCAL_ADDRESS = "127.0.0.1";
 
     try {
-        let geoip = null;
+        let geoip = await geoIpService.findByIp(req.socket.remoteAddress);
 
-        if (req.socket.remoteAddress !== LOCAL_ADDRESS){
-            geoip = await geoIpService.findByIp(req.socket.remoteAddress);
-
-            if (geoip == null){
-                geoip = await geoIpService.getInfo(req.socket.remoteAddress);
-            }
+        if (geoip == null){
+            await geoIpService.getInfo(req.socket.remoteAddress);
         }
     }
     catch (e){
